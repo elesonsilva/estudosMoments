f14e28af4dbfc8ad1a6051913aac274eeb3cf695
Éleson Silva <elesonsilva9@gmail.com>
2023-12-28 11:04:09 -0300

    finalizando o style da tela de momento idividual

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -3,16 +3,22 @@ import { CommonModule } from '@angular/common';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MomentService } from '../../../services/moment.service';
 import { Moment } from '../../../Moments';
+import { environment } from '../../../../environments/environment';
+import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-moment',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './moment.component.html',
   styleUrl: './moment.component.css'
 })
 export class MomentComponent {
   moment?:Moment;
+  baseApiUrl= environment.Url
+  faTimes = faTimes;
+  faEdit = faEdit;
 
   constructor(
     private momentService:MomentService,
