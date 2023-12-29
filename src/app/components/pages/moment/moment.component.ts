c844cfb9820cad927172b9ac17d45a7ae61ebb49
Éleson Silva <elesonsilva9@gmail.com>
2023-12-29 11:48:02 -0300

    criando serviço de comentarios

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -7,11 +7,16 @@ import { environment } from '../../../../environments/environment';
 import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MessagesService } from '../../../services/messages.service';
+import { Coment } from '../../../Coments';
+import { ComentService } from '../../../services/coment.service';
+import { FormGroup, FormControl,Validators, FormGroupDirective } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-moment',
   standalone: true,
-  imports: [CommonModule, FontAwesomeModule, RouterLink],
+  imports: [CommonModule, FontAwesomeModule, RouterLink,ReactiveFormsModule],
   templateUrl: './moment.component.html',
   styleUrl: './moment.component.css'
 })
@@ -21,25 +26,40 @@ export class MomentComponent {
   faTimes = faTimes;
   faEdit = faEdit;
 
+  comentForm!: FormGroup
+
   constructor(
     private momentService:MomentService,
     private route: ActivatedRoute,
     private messageService: MessagesService,
     private Router: Router,
+    private comentService: ComentService,
     ){}
 
   ngOnInit():void{
     //id que esta na url
     const id = Number(this.route.snapshot.paramMap.get('id'))
     this.momentService.getMoment(id).subscribe((item) => (this.moment = item.data))
-  }
 
+    this.comentForm = new FormGroup({
+      text: new FormControl("",[Validators.required]),
+      username: new FormControl("",[Validators.required])
+    });
+  }
+  get text(){
+    return this.comentForm.get('text')!
+  }
+  get username(){
+    return this.comentForm.get('username')!
+  }
 
   async removeHendler(id: Number){
     await this.momentService.removeMoment(id).subscribe();
     this.messageService.add("Momento excluido com sucesso");
     this.Router.navigate(['/']);
   }
+
+  onSubmit(FormDirective: FormGroupDirective){}
       
 
 }
--- src/app/services/coment.service.ts
+++ src/app/services/coment.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Coment } from '../Coments';
+import { Response } from '../Response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComentService {
+  private baseApiUrl = environment.Url
+  private apiUrl = `${this.baseApiUrl}api/moments/coments`
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  creatComent(data:Coment):Observable<Response<Coment>>{
+    return this.http.post<Response<Coment>>(this.apiUrl, data)
+  }
 }
